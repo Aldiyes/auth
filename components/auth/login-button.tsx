4d0cbbefa724b63eaa55f85b9073058713bc9428
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2025-05-03 04:26:58 +0700

    create login button component and redirect to /auth/login

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,3 +1,22 @@
-export const LoginButton = () => {
-	return <div>Login Button</div>;
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+type LoginButtonProps = {
+	children: React.ReactNode;
+	mode?: 'modal' | 'redirect';
+	asChild?: boolean;
+};
+
+export const LoginButton = ({ children, mode, asChild }: LoginButtonProps) => {
+	const router = useRouter();
+
+	const onClick = () => {
+		router.push('/auth/login');
+	};
+
+	if (mode === 'modal') {
+		return <span>TODO: Implement modal</span>;
+	}
+	return <span onClick={onClick}>{children}</span>;
 };
